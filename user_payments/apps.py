f2d49a327d30011aa2faa735189a372b319e5904
Matthias Kestenholz <mk@feinheit.ch>
2018-05-18 14:50:26 +0000

    No point waiting, just do it already

--- user_payments/apps.py
+++ user_payments/apps.py
@@ -2,7 +2,6 @@ from datetime import timedelta
 from types import SimpleNamespace
 
 from django.apps import AppConfig
-from django.utils.functional import cached_property
 from django.utils.text import capfirst
 from django.utils.translation import gettext_lazy as _
 
@@ -13,10 +12,9 @@ class UserPayments(AppConfig):
 
     default_settings = {"currency": "CHF", "grace_period": timedelta(days=3)}
 
-    @cached_property
-    def settings(self):
+    def ready(self):
         from django.conf import settings
 
-        return SimpleNamespace(
+        self.settings = SimpleNamespace(
             **{**self.default_settings, **getattr(settings, "USER_PAYMENTS", {})}
         )
