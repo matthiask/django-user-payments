e897d47565a480442db752db059ddd151062f51c
Matthias Kestenholz <mk@feinheit.ch>
2018-05-18 13:59:30 +0000

    First stripe (test) payment

--- user_payments/processors.py
+++ user_payments/processors.py
@@ -6,6 +6,7 @@ from django.core.mail import send_mail
 from django.utils import timezone
 
 import stripe
+from mooch.signals import post_charge
 from user_payments.models import LineItem, Payment
 from user_payments.stripe_customers.models import Customer
 
@@ -22,24 +23,24 @@ def attempt_using_stripe_customers(payment):
     try:
         charge = stripe.Charge.create(
             customer=customer.customer_id,
-            amount=payment.instance.amount_cents,
+            amount=payment.amount_cents,
             currency="CHF",
             idempotency_key=payment.id.hex,
         )
 
     except stripe.CardError as exc:
-        # TODO do a thing...
-        logger.exception(exc)
+        logger.exception("Failure charging the customers' card")
+        send_mail(str(payment), str(exc), None, [payment.email], fail_silently=True)
         return False
 
     else:
-        # TODO check for more errors?
+        payment.payment_service_provider = 'user-payments-stripe-customers'
         payment.charged_at = timezone.now()
         payment.transaction = str(charge)
         payment.save()
 
-        # FIXME sender? request?
-        # post_charge.sender(sender=self, payment=instance, request=None)
+        # FIXME sender?
+        post_charge.send(sender=attempt_using_stripe_customers, payment=payment, request=None)
 
         return True
 
@@ -49,9 +50,7 @@ def send_notification_mail(payment):
     send_mail(str(payment), "<No body>", None, [payment.email], fail_silently=True)
 
 
-default_processors = [
-    attempt_using_stripe_customers, send_notification_mail
-]
+default_processors = [attempt_using_stripe_customers, send_notification_mail]
 
 
 def process_unbound_items(*, processors=default_processors):
