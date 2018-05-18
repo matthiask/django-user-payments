89f5229410eaff071c96bd3f8b0e91dace2cfacd
Matthias Kestenholz <mk@feinheit.ch>
2018-05-18 17:45:29 +0200

    Reformat Payment.description

--- user_payments/models.py
+++ user_payments/models.py
@@ -59,7 +59,11 @@ class Payment(AbstractPayment):
 
     @property
     def description(self):
-        return "Payment of %s by %s: %s" % (self.amount, self.email, ", ".join(str(item) for item in self.lineitems.all()))
+        return "Payment of %s by %s: %s" % (
+            self.amount,
+            self.email,
+            ", ".join(str(item) for item in self.lineitems.all()),
+        )
 
 
 class LineItemQuerySet(models.QuerySet):
