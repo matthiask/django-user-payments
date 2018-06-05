ec8e3f54ae90b5e91cc92f0b162aeab9ae43c5b9
Matthias Kestenholz <mk@feinheit.ch>
2018-06-05 16:03:25 +0200

    Remove the process_payments management command which does not work anymore

--- user_payments/user_subscriptions/management/commands/process_subscriptions.py
+++ user_payments/user_subscriptions/management/commands/process_subscriptions.py
@@ -1,11 +1,12 @@
-from user_payments.management.base import OptionalRavenCommand
+from django.core.management.base import BaseCommand
+
 from user_payments.user_subscriptions.models import Subscription, SubscriptionPeriod
 
 
-class Command(OptionalRavenCommand):
+class Command(BaseCommand):
     help = "Generate subscription periods and user payments line items"
 
-    def _handle(self, **options):
+    def handle(self, **options):
         Subscription.objects.disable_autorenewal()
         Subscription.objects.create_periods()
         SubscriptionPeriod.objects.create_line_items()
